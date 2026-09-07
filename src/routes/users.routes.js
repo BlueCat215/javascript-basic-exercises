0d3a3cc2c3a0f8b0de500dc8256f6d2d4cb5d024
hieu <anhhieuk746@gmail.com>
2026-09-07 16:13:00 +0700

    refactor(users): sửa lỗi ép kiểu id người dùng và đồng bộ hóa các API quản lý tài khoản

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,3 @@
-// src/routes/users.routes.js
 const express = require("express");
 const JsonCollection = require("../db");
 const { authenticateToken, authorizeRoles } = require("../middleware/auth");
